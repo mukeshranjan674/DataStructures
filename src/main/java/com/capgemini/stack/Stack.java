bc83b0153a8848766dcc71697113682101045941
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-14 23:28:51 +0530

    [Mukesh] add : ability to create a stack using linked list

--- src/main/java/com/capgemini/stack/Stack.java
+++ src/main/java/com/capgemini/stack/Stack.java
@@ -1,5 +1,23 @@
 package com.capgemini.stack;
 
+import com.capgemini.linkedlist.INode;
+import com.capgemini.linkedlist.LinkedList;
+
 public class Stack {
 
+	private LinkedList linkedList;
+	
+	public Stack() {
+		linkedList = new LinkedList();
+	}
+
+	public void push(INode firstNode) {
+		linkedList.add(firstNode);
+	}
+	public INode peek() {
+		return linkedList.head;
+	}
+	public void printStack() {
+		linkedList.printNodes();
+	}
 }
--- src/test/java/com/capgemini/stack/StackTest.java
+++ src/test/java/com/capgemini/stack/StackTest.java
@@ -1,5 +1,25 @@
 package com.capgemini.stack;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.capgemini.linkedlist.INode;
+import com.capgemini.linkedlist.Node;
+
 public class StackTest {
 
+	@Test
+	public void whenGiven3NumbersShouldReturnAStack() {
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		Stack stack = new Stack();
+		stack.push(firstNode);
+		stack.push(secondNode);
+		stack.push(thirdNode);
+		INode result = stack.peek();
+		stack.printStack();
+		assertEquals(thirdNode, result);
+	}
 }
