8cc567226b4919df764c2be9bf56e0630264e919
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-15 00:11:08 +0530

    [Mukesh] add : Ability to Enqueue using Linked List

--- src/main/java/com/capgemini/queue/Queue.java
+++ src/main/java/com/capgemini/queue/Queue.java
@@ -1,5 +1,25 @@
 package com.capgemini.queue;
 
+import com.capgemini.linkedlist.INode;
+import com.capgemini.linkedlist.LinkedList;
+
 public class Queue {
 
+	private LinkedList linkedList;
+
+	public Queue() {
+		linkedList = new LinkedList();
+	}
+
+	public void enqueue(INode newNode) {
+		linkedList.append(newNode);
+	}
+
+	public void printQueue() {
+		linkedList.printNodes();
+	}
+
+	public INode peek() {
+		return linkedList.head;
+	}
 }
--- src/test/java/com/capgemini/queue/QueueTest.java
+++ src/test/java/com/capgemini/queue/QueueTest.java
@@ -1,5 +1,28 @@
 package com.capgemini.queue;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.capgemini.linkedlist.INode;
+import com.capgemini.linkedlist.Node;
+
 public class QueueTest {
 
+	/**
+	 * UC1
+	 */
+	@Test
+	public void whenGiven3NumbersShouldReturnAStack() {
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		Queue queue = new Queue();
+		queue.enqueue(firstNode);
+		queue.enqueue(secondNode);
+		queue.enqueue(thirdNode);
+		INode result = queue.peek();
+		queue.printQueue();
+		assertEquals(firstNode, result);
+	}
 }
