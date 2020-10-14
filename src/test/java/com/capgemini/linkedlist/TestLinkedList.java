16e5398ea815fa482b90b5d7339415d8f3fa7755
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-14 11:13:25 +0530

    [Mukesh] add : Node class and TestLinkedList class written

---
+++ src/test/java/com/capgemini/linkedlist/TestLinkedList.java
@@ -0,0 +1,14 @@
+package com.capgemini.linkedlist;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestLinkedList {
+
+	@Test
+	public void given3NumbersShouldAddToLinkedList() {
+		
+	}
+
+}
