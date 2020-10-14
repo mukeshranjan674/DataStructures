465a70b0a6dad7e13794d7bc1c1c70462a24304c
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-14 11:16:04 +0530

    [Mukesh] add : integer linked list created with values 56 30 70

--- src/main/java/com/capgemini/linkedlist/Node.java
+++ src/main/java/com/capgemini/linkedlist/Node.java
@@ -2,4 +2,27 @@ package com.capgemini.linkedlist;
 
 public class Node<K> {
 
+	private K key;
+	private Node next;
+
+	public Node(K key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
--- src/test/java/com/capgemini/linkedlist/TestLinkedList.java
+++ src/test/java/com/capgemini/linkedlist/TestLinkedList.java
@@ -8,7 +8,13 @@ public class TestLinkedList {
 
 	@Test
 	public void given3NumbersShouldAddToLinkedList() {
-		
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+		assertTrue(result);
 	}
 
 }
